91a25dc16db9c368e6b27a51aa9d8c33e0474051
Shin Andrei Riego <riegoshinandrei@gmail.com>
2022-10-11 18:05:52 +0800

    Add headers

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const getPage = async (url) => {
   const name = url.replace(/^\//, '');
   const filename = url === '/' ? 'index.html' : name + '.html';
 
-  try {
-    return await fs.readFile(path.join('pages', filename), 'utf-8');
-  } catch (err) {
-    console.log(err);
-    return await fs.readFile(path.join('pages', '404.html'), 'utf-8');
-  }
+  return await fs.readFile(path.join('pages', filename), 'utf-8');
 };
 
 const server = http.createServer(async (req, res) => {
-  const page = await getPage(req.url);
+  res.setHeader('Content-Type', 'text/html');
+
+  let page;
+  try {
+    page = await getPage(req.url);
+  } catch (error) {
+    res.statusCode = 404;
+    res.statusMessage = 'Not found';
+    page = await getPage('404');
+  }
+
   res.end(page);
 });
 
