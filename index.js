a97d1fb6b6544adc53b855e00cd25599d0acd9ae
Shin Andrei Riego <riegoshinandrei@gmail.com>
2022-10-11 17:50:56 +0800

    Complete project specs

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const fs = require('fs/promises');
 const http = require('http');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello, world!');
+const getPage = async (url) => {
+  const name = url.replace(/^\//, '');
+  const filename = url === '/' ? 'index.html' : name + '.html';
+
+  try {
+    return await fs.readFile(path.join('pages', filename), 'utf-8');
+  } catch (err) {
+    console.log(err);
+    return await fs.readFile(path.join('pages', '404.html'), 'utf-8');
+  }
+};
+
+const server = http.createServer(async (req, res) => {
+  const page = await getPage(req.url);
+  res.end(page);
 });
 
 server.listen(8080, '127.0.0.1', () => {
