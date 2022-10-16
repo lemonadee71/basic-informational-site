fd8842b5274a061159feaefcfd4b824f2a31af56
Shin Andrei Riego <riegoshinandrei@gmail.com>
2022-10-16 20:57:23 +0800

    Complete refactor code to use express

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const fs = require('fs/promises');
-const http = require('http');
 const path = require('path');
 const express = require('express');
 
@@ -7,15 +5,11 @@ const app = express();
 app.use(require('morgan')('dev'));
 app.use(express.static('public'));
 
-const getPage = async (url) => {
-  const filename = url ? url + '.html' : 'index.html';
+const file = (filename) => path.join(__dirname, 'pages', filename);
 
-  return fs.readFile(path.join('pages', filename), 'utf-8');
-};
-
-app.get('/', async (req, res) => res.send(await getPage()));
-app.get('/about', async (req, res) => res.send(await getPage('about')));
-app.get('/contact', async (req, res) => res.send(await getPage('contact')));
-app.use(async (req, res) => res.status(404).send(await getPage('404')));
+app.get('/', (req, res) => res.sendFile(file('index.html')));
+app.get('/about', (req, res) => res.sendFile(file('about.html')));
+app.get('/contact', (req, res) => res.sendFile(file('contact.html')));
+app.use((req, res) => res.status(404).sendFile(file('404.html')));
 
 app.listen(8080, () => console.log('Server is listening to port 8080'));
